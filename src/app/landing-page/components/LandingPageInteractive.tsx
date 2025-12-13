25525e4f69021a3364dfc8f4a15695e7f75135ed
Scraping_CleaningData <conti.sola93@gmail.com>
2025-12-13 16:11:52 -0300

    arreglo de wpp

--- src/app/landing-page/components/LandingPageInteractive.tsx
+++ src/app/landing-page/components/LandingPageInteractive.tsx
@@ -46,7 +46,7 @@
     };
 
     const handleWhatsAppClick = () => {
-        window.open('https://wa.me/1234567890', '_blank');
+        window.open('https://wa.me/+59893892924', '_blank');
     };
 
     if (!isHydrated) {
